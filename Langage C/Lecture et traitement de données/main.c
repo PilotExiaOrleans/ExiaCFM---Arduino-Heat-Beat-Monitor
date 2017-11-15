3e332fd7bcd92fbfd9dfa083a367e81ea04a8bcc
Louis Marjolet <33122779+LoLouis10@users.noreply.github.com>
2017-11-15 17:20:21 +0100

    ajout debut module 3.4
    lecture et traitement de données, début

---
+++ Langage C/Lecture et traitement de données/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choix;
+    printf("Coucou mes petits bichons, alors aujourd'hui on se retrouve pour un unboxing de mascara!!!\n");
+    menu(choix);
+    return 0;
+}
---
+++ Langage C/Lecture et traitement de données/menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
+
+void menu(int choix)
+{
+    printf("Ouverture du menu\n\n0 - Affiche toutes les LEDs dans l'ordre du fichier\n1 - Affiche les donn%ces dans l'ordre croissant ou d%ccroissant\n", 130, 130);
+    printf("2 - Affiche les donn%ces en fonction du temps\n3 - Donne la moyenne du pouls\n", 130);
+    printf("4 - Affiche le nombre de lignes de donn%ces actuellement en m%cmoire\n5 - Affiche les maximum et minimums du pouls\n", 130, 130);
+    scanf("%d", &choix);
+    switch (choix)
+    {
+        case 0 : Donnee_dans_l_ordre_du_fichier();break;
+		case 1 : Ordre_croissant_ou_decroissant();break;
+		case 2 : Rechercher_et_afficher_donnees_en_fonction_du_temps();break;
+		case 3 : Moyenne_pouls();break;
+		case 4 : Afficher_nbr_lignes_de_donnees();break;
+		case 5 : Max_et_Min_pouls();break;
+        default : return 0;
+    }
+}
+
