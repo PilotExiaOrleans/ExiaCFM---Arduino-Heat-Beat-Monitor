4670adabc4b3cb3e0d7304a994326c7cd1b5bb44
PhilippeBurlet <philippe.burlet@viacesi.Fr>
2017-11-16 15:03:55 +0100

    Modification Action.c
    Ajout de la fonction tri à bulles

--- Langage C/Lecture et traitement de données/actions.c
+++ Langage C/Lecture et traitement de données/actions.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAILLE_MAX 5010 //Taille du tableau à 5010
 
-/*void tri_bulles(int tab[])
+void tri_bulles(int tab[])
 {
     short tab_en_ordre = 0;
     int taille = sizeof(tab);
+    int i;
+    int temp;
     while(!tab_en_ordre)
     {
         tab_en_ordre = 1;
@@ -14,22 +15,42 @@
         {
             if(tab[i] > tab[i+1])
             {
-                swap(tab[i],tab[i+1]);
+                temp = tab[i];
+                tab[i] = tab[i+1];
+                tab[i+1] = temp;
                 tab_en_ordre = 0;
             }
         }
         taille--;
     }
-}*/
 
+}
+
+
+
+/*
 void Donnee_dans_l_ordre_du_fichier()
 {
-    char tab[TAILLE_MAX];
-    printf("%s", tab);
+
 }
-/*void Ordre_croissant_ou_decroissant()
+void Ordre_croissant_ou_decroissant()
+{
+
+}
 void Rechercher_et_afficher_donnees_en_fonction_du_temps()
+{
+
+}
 void Moyenne_pouls()
+{
+
+}
 void Afficher_nbr_lignes_de_donnees()
+{
+
+}
 void Max_et_Min_pouls()
+{
+
+}
 */
