64921f6e5ed5b701d90dd1eca4a467225ffb6285
Joël Didier <didier.joel@orange.fr>
2017-11-16 15:40:10 +0100

    Update

---
+++ # DOSSIER FINAL #/Modules/M2_Coeur_LEDs/Generateur_Code.h
@@ -0,0 +1,13 @@
+#ifndef GENERATEUR_CODE_H_INCLUDED
+#define GENERATEUR_CODE_H_INCLUDED
+
+    void Initialisation()
+    void Ouverture_Televersement()
+    void Toutes_les_LEDs_allumees()
+    void Une_LED_sur_2_allumee()
+    void Une_LED_sur_3_allumee()
+    void Une_LED_allumee_au_choix()
+    void Chenille()
+    void Chargement()
+
+#endif // GENERATEUR_CODE_H_INCLUDED
--- # DOSSIER FINAL #/Modules/M2_Coeur_LEDs/menu.c
+++ # DOSSIER FINAL #/Modules/M2_Coeur_LEDs/menu.c
@@ -8,15 +8,18 @@ void menu(int choix)
     printf("Ouverture du menu\n\n0 - Toutes les LEDs s'allument au rythme du pouls de la personne\n1 - Une LED sur 2 s'allume\n");
     printf("2 - Une LED sur 3 s'allume\n3 - Choisissez quelle LED va s'allumer\n");
     printf("4 - Allumage sous forme de chenille\n5 - Affichage de haut en bas\n");
+
+    // Entree du choix par l'utilisateur
     scanf("%d", &choix);
+    // Choix et appel fonction correspondante
     switch (choix)
     {
-        case 0 : Toutes_les_LEDs_allumees();break;
+        case 0 : Toutes_les_LEDs_allumees();break; 
 		case 1 : Une_LED_sur_2_allumee();break;
 		case 2 : Une_LED_sur_3_allumee();break;
 		case 3 : Une_LED_allumee_au_choix();break;
 		case 4 : Chenille();break;
 		case 5 : Chargement();break;
-        default : return 0;
+        default : return 0; // Fin
     }
 }
