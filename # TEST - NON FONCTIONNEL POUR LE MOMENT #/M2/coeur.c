5220e853c30fe5a52897952bdf9a15cb81ccbace
Joël Didier <didier.joel@orange.fr>
2017-11-16 23:16:47 +0100

    Updated coeur.c (more LED blink)

--- # TEST - NON FONCTIONNEL POUR LE MOMENT #/M2/coeur.c
+++ # TEST - NON FONCTIONNEL POUR LE MOMENT #/M2/coeur.c
@@ -15,19 +15,45 @@ void AffichagePulsations (int ???)
 
 void Toutes_les_LEDs_allumees(???)
 {
-
+    int i;
+    for(i = 2; i <= 11 ; i++)
+    {
+      if(??? > 500)
+      {
+        digitalWrite(i, HIGH);
+      }
+      else
+      {
+        digitalWrite(i, LOW);
+      }
 }
 
 // Generation param.h 1 LED sur 2 Allumee
 void Une_LED_sur_2_allumee(???)
 {
-
+  int i;
+  int delais=75;
+  for (int i=2;i<12;i+=2)
+  {
+    digitalWrite(i, HIGH);
+    delay(delais);
+    digitalWrite(i, LOW);
+  }
 }
 
 // Generation param.h 1 LED sur 3 Allumee
 void Une_LED_sur_3_allumee(???)
 {
-  
+  int i;
+  int delais=75;
+  while( i<12)
+  {
+    digitalWrite(i, HIGH);
+    delay(delais);
+    digitalWrite(i, LOW); 
+    if (i>8){i-=7;}
+    else{ i+=3;
+  }
 }
 
 // Generation param.h 1 LED Allumee Au Choix
@@ -39,11 +65,46 @@ void Une_LED_allumee_au_choix(???)
 // Generation param.h Mode Chenille
 void Chenille(???)
 {
+  int i;
+  int delais;
+  for (int i=2;i<12;i+=1)
+  {
+    digitalWrite(i, HIGH); 
+    delay(delais);
+    digitalWrite(i, LOW);
+  }
   
 }
 
 // Generation param.h Mode Chargement
 void Chargement(???)
 {
-   
+  int delais = 75;
+  
+    for (int i=2 ; i<8 ; i++)
+    {
+      if (i==2 || i==7)
+      {
+        digitalWrite(i, HIGH);
+      }
+      else
+      {
+        digitalWrite(i, HIGH);
+        digitalWrite(14-i, HIGH);
+      }
+      delay(delais);
+    }
+    for (int i=7 ; i>1 ; i--)
+    {
+      if (i==2 || i==7)
+      {
+        digitalWrite(i, LOW);
+      }
+      else
+      {
+        digitalWrite(i, LOW);
+        digitalWrite(14-i, LOW);
+      }
+      delay(delais);
+    }
 }
