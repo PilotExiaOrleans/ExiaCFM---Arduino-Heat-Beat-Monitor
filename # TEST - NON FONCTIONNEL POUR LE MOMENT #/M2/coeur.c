0ca2e965f32ccf4f9b69acc2915cb5b497b6eb7c
Joël Didier <didier.joel@orange.fr>
2017-11-16 22:23:25 +0100

    (Near the end !) Add template for led blinking

---
+++ # TEST - NON FONCTIONNEL POUR LE MOMENT #/M2/coeur.c
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include "coeur.h"
+#include "param.h"
+
+void AffichagePulsations (int ???)
+{
+    switch(LEDPARAM)
+    case 1:Toutes_les_LEDs_allumees(???);break;
+    case 2:Une_LED_sur_2_allumee(???);break;
+    case 3:Une_LED_sur_3_allumee(???);break;
+    case 4:Une_LED_allumee_au_choix(???);break;
+    case 5:Chenille(???);break;
+    case 6:Chargement(???);break;
+}
+
+void Toutes_les_LEDs_allumees(???)
+{
+
+}
+
+// Generation param.h 1 LED sur 2 Allumee
+void Une_LED_sur_2_allumee(???)
+{
+
+}
+
+// Generation param.h 1 LED sur 3 Allumee
+void Une_LED_sur_3_allumee(???)
+{
+  
+}
+
+// Generation param.h 1 LED Allumee Au Choix
+void Une_LED_allumee_au_choix(???)
+{
+  
+}
+
+// Generation param.h Mode Chenille
+void Chenille(???)
+{
+  
+}
+
+// Generation param.h Mode Chargement
+void Chargement(???)
+{
+   
+}
