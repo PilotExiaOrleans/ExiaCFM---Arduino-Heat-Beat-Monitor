0ad207521051b5a985b46570cada2272db06472a
Louis Marjolet <33122779+LoLouis10@users.noreply.github.com>
2017-11-17 15:13:37 +0100

    mise a jour du menu
    ajout d'une boucle while

--- Livrables/Modules/M4_Lecture_Traitement_Donnees/menu.c
+++ Livrables/Modules/M4_Lecture_Traitement_Donnees/menu.c
@@ -7,22 +7,28 @@
 void menu(structure tab[],int n)
 {
     int choix;
-    printf("Ouverture du menu\n\n0 - Affiche toutes les LEDs dans l'ordre du fichier\n1 - Affiche les donn%ces dans l'ordre croissant ou d%ccroissant\n", 130, 130);
-    printf("2 - Affiche les donn%ces en fonction du temps\n3 - Donne la moyenne du pouls\n", 130);
-    printf("4 - Affiche le nombre de lignes de donn%ces actuellement en m%cmoire\n5 - Affiche les maximum et minimums du pouls\n", 130, 130);
-    printf("6 - Quitter l'application\n\n");
-    scanf("%d", &choix);    //Demande à l'utilisateur de choisir le mode d'affichage
-    switch (choix)          //Appel de la fonction voulue
+    while (choix!=6)
     {
-        case 0 : Donnee_ordre_du_fichier(tab, n);break;
-		case 1 : Ordre_croissant_ou_decroissant(tab, n);break;
-		case 2 : Rechercher_et_afficher_donnees_en_fonction_du_temps(tab, n);break;
-		case 3 : Moyenne_pouls(tab, n);break;
-		case 4 : Afficher_nbr_lignes_de_donnees(tab, n);break;
-		case 5 : Max_et_Min_pouls(tab, n);break;
-		case 6 : return 0;
-        default :printf("Le choix n'est pas valide");
-                 return 0;
+        printf("Ouverture du menu\n\n0 - Affiche toutes les LEDs dans l'ordre du fichier\n1 - Affiche les donn%ces dans l'ordre croissant ou d%ccroissant\n", 130, 130);
+        printf("2 - Affiche les donn%ces en fonction du temps\n3 - Donne la moyenne du pouls\n", 130);
+        printf("4 - Affiche le nombre de lignes de donn%ces actuellement en m%cmoire\n5 - Affiche les maximum et minimums du pouls\n", 130, 130);
+        printf("6 - Quitter l'application\n\n");
+
+        scanf("%d", &choix);    //Demande à l'utilisateur de choisir le mode d'affichage
+        system("cls");
+        switch (choix)          //Appel de la fonction voulue
+        {
+            case 0 : Donnee_ordre_du_fichier(tab, n);break;
+            case 1 : Ordre_croissant_ou_decroissant(tab, n);break;
+            case 2 : Rechercher_et_afficher_donnees_en_fonction_du_temps(tab, n);break;
+            case 3 : Moyenne_pouls(tab, n);break;
+            case 4 : Afficher_nbr_lignes_de_donnees(tab, n);break;
+            case 5 : Max_et_Min_pouls(tab, n);break;
+            case 6 : return 0;
+            default : printf("Le choix n'est pas valide\n");
+        }
+        system("PAUSE");
+        system("cls");
     }
 }
 
