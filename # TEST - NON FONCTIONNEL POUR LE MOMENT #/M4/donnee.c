f870f970dc1fb001fcda85de50b337afb47e52be
Louis Marjolet <33122779+LoLouis10@users.noreply.github.com>
2017-11-17 12:57:44 +0100

    beaucoup de changement dans module 3
    Bon fonctionnement, a perfectionner

--- # TEST - NON FONCTIONNEL POUR LE MOMENT #/M4/donnee.c
+++ # TEST - NON FONCTIONNEL POUR LE MOMENT #/M4/donnee.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define TAILLE_MAX 5000 //Taille du tableau � 5010
+#include "donnee.h"
 
-
-void donnee()
+void RemplirTableau(structure tableau[], int nb)
 {
     FILE* fichier = NULL; // initialisation du fichier
-    char tab[TAILLE_MAX]; //initialisation du tableau permettant la lecture du fichier
+    char chaine[nb]; //initialisation du tableau permettant la lecture du fichier
     int i=0, pls; // i=compteur de boucle       pls= pouls
     float tps; //tps=temps
-    struct Tableau
-    {
-        float temps; //définition de nouveaux types de variables
-        int pouls;
-    };
 
-
-    struct Tableau tableau[TAILLE_MAX]; //création d'un tableau pour stocker nos 2 donnees
     fichier = fopen("jeu_de_donnees_test.csv", "r"); // ouverture du fichier en lecture seule
 
     if (fichier != NULL) // verification de l'ouverture du fichier
     {
-        while (fgets(tab, TAILLE_MAX, fichier) != NULL) //lire le fichier ligne par ligne tant qu'on est pas a la fin
+        while (fgets(chaine, nb, fichier) != NULL) //lire le fichier ligne par ligne tant qu'on est pas a la fin
         {
             //printf("%s", tab); // test pour voir si le tableau "tab" fonctionne
 
             //on stock le poul et le temps dans des variables tampons
-            sscanf(tab, "%f;%d", &tps, &pls); //lecture de la ligne du fichier (c'est ca qui marche pas)
+            sscanf(chaine, "%f;%d", &tps, &pls); //lecture de la ligne du fichier (c'est ca qui marche pas)
             //printf("%f %d\n", tps, pls);
             //puis on le stock dans le tableau de structures
 
                 tableau[i].pouls = pls;
                 tableau[i].temps = tps;
 
-            printf("Temps: %f, Pouls: %d\n", tableau[i].temps, tableau[i].pouls); //affichage des donnees
+            //printf("Temps: %f, Pouls: %d\n", tableau[i].temps, tableau[i].pouls); //affichage des donnees
             i++; //incrementation de i*/
             if(i>5000)break;
         }
