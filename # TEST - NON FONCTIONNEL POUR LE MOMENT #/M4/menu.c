46fb763680351d03928318adb020e7e90426802b
Trapadore <vaz.vicente.mc@gmail.com>
2017-11-17 11:02:25 +0100

    Ajout fonctionnalité quitter l'application dans le module 4

--- # TEST - NON FONCTIONNEL POUR LE MOMENT #/M4/menu.c
+++ # TEST - NON FONCTIONNEL POUR LE MOMENT #/M4/menu.c
@@ -10,6 +10,7 @@ void menu(structure tab[],int n)
     printf("Ouverture du menu\n\n0 - Affiche toutes les LEDs dans l'ordre du fichier\n1 - Affiche les donn%ces dans l'ordre croissant ou d%ccroissant\n", 130, 130);
     printf("2 - Affiche les donn%ces en fonction du temps\n3 - Donne la moyenne du pouls\n", 130);
     printf("4 - Affiche le nombre de lignes de donn%ces actuellement en m%cmoire\n5 - Affiche les maximum et minimums du pouls\n", 130, 130);
+    printf("6 - Quitter l'application\n\n");
     scanf("%d", &choix);    //Demande à l'utilisateur de choisir le mode d'affichage
     switch (choix)          //Appel de la fonction voulue
     {
@@ -19,7 +20,9 @@ void menu(structure tab[],int n)
 		case 3 : Moyenne_pouls(tab, n);break;
 		case 4 : Afficher_nbr_lignes_de_donnees(tab, n);break;
 		case 5 : Max_et_Min_pouls(tab, n);break;
-        default : return 0;
+		case 6 : return 0;
+        default :printf("Le choix n'est pas valide");
+                 return 0;
     }
 }
 
