f5fdb53ee65a14fe35c58a32f144cc4560dbe4a9
Trapadore <vaz.vicente.mc@gmail.com>
2017-11-20 09:41:59 +0100

    correction simulateur.c

--- Livrables/Modules/M1_Cardio/main/simulateur.c
+++ Livrables/Modules/M1_Cardio/main/simulateur.c
@@ -9,7 +9,7 @@ Exia CESI A1 - Orleans
 */
 
 int Simulation(int repere,int repere_const)        //Dans cette fonction, nous simulons des valeurs basses et hautes à intervalle régulier correspondant à la durée d'un battement et l'écart entre deux battements.
-{                                 //Cet intervalle est aléaoire et importe peu, il suffit juste qu'il soit assez grand pour être identifiable par le système
+{                                                  //Cet intervalle est aléaoire et importe peu, il suffit juste qu'il soit assez grand pour être identifiable par le système
   int m = 0;
   if (repere < (repere_const/2))                //Si repere est inferieur  à 25 (valeur aleatoire), on insere des valeurs correspondant à un etat bas
   { 
